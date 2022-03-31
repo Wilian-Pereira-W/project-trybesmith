596b194751c04f38d5bc21d3259a93e9dc6129ec
Wilian Pereira <wilian_pereira92@outlook.com>
2022-03-31 16:59:54 -0300

    feat: adicionando a rota get products

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import ProductsControllers from './controllers/ProductsController';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/products', ProductsControllers.getAll);
 
 export default app;
