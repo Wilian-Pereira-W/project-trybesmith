eac328b2ef54a79399c1a25a13b7ff7df017fd48
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-01 18:22:04 -0300

    feat: rota do create

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import ProductsControllers from './controllers/ProductsController';
+import validateProduct from './middleware/validateProduct';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/products', ProductsControllers.getAll);
+app.post(
+  '/products', 
+  validateProduct.validateName, 
+  validateProduct.validateAmount,
+  ProductsControllers.create,
+);
 
 export default app;
