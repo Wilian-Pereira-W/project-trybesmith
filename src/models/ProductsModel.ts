7e1788ac5e1c3b0bff4458946683785308f47c05
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-01 18:18:29 -0300

    feat: implementando o model do create

--- src/models/ProductsModel.ts
+++ src/models/ProductsModel.ts
@@ -1,13 +1,25 @@
-import { RowDataPacket } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import connection from './connection';
 import { Product } from '../interfaces/Product';
 
-const getAll : () => Promise<Product[]> = async () => { 
+const getAll = async (): Promise<Product[]> => { 
   const result = await connection.execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Products');
   const [rows] = result;
   return rows as Product[];
 };
 
+const create = async (product: Product): Promise<Product> => {
+  const { name, amount } = product;
+  const [result] = await connection.execute<ResultSetHeader>(
+    'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+    [name, amount],
+  );
+  const { insertId } = result;
+
+  return { id: insertId, ...product };
+};
+
 export default {
   getAll,
+  create,
 };
