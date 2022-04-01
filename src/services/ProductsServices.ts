9c57fffcdd9d9dbfd60e06839ba651508e3aff8b
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-01 18:19:04 -0300

    feat: implementando o service do create do product

--- src/services/ProductsServices.ts
+++ src/services/ProductsServices.ts
@@ -1,11 +1,18 @@
 import { Product } from '../interfaces/Product';
 import ProductsModel from '../models/ProductsModel';
 
-const getAll: () => Promise<Product[]> = async () => {
+const getAll = async (): Promise<Product[]> => {
   const products = await ProductsModel.getAll();
   return products;
 };
 
+const create = async (product: Product): Promise<Product> => {
+  const { name, amount } = product;
+  const createProduct = await ProductsModel.create({ name, amount });
+  return createProduct;
+};
+
 export default {
   getAll,
+  create,
 };
