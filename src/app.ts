d287d7bab1b916ba23ae2f220001cbe7b829e1b0
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-04 09:35:46 -0300

    feat: implementando a rota do user

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import ProductsControllers from './controllers/ProductsController';
+import UsersController from './controllers/UsersController';
 import validateProduct from './middleware/validateProduct';
+import validateUser from './middleware/validateUser';
 
 const app = express();
 
@@ -13,5 +15,12 @@ app.post(
   validateProduct.validateAmount,
   ProductsControllers.create,
 );
-
+app.post(
+  '/users',
+  validateUser.validateUsername,
+  validateUser.validateClasse,
+  validateUser.validateLevel,
+  validateUser.validatePassword,
+  UsersController.create,
+);
 export default app;
